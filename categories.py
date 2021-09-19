273a38302e0a0ba7ec089c95c57dd8671cb90e43
Erick Silva <erick.ap.silva@gmail.com>
2021-09-19 12:41:44 -0300

    Categories classification improvement

--- categories.py
+++ categories.py
@@ -2,8 +2,13 @@
 
 import db
 import re
+import sys
 
-categs=db.query('select * from CATEGORY')
+if '--reset' in sys.argv:
+    db.query('update LOG set `CATEGORY`=null')
+    sys.exit(0)
+
+categs=db.query('select * from CATEGORY order by `PRIORITY`, `ID`')
 logs=db.query('select * from LOG where CATEGORY is null')
 
 for log in logs:
@@ -13,6 +18,7 @@ for log in logs:
         if re.match(r,log[1]):
             db.query("update LOG set `CATEGORY`='%s' where `ID`=%d" % (cat[2], log[0]))
             match=True
+            break
         if not match:
             print(log)
 
