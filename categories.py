48ad9cbddea2162090fa8112dce41d8b25542e26
Erick Silva <erick.ap.silva@gmail.com>
2021-09-19 11:47:47 -0300

    Categories update engine development

--- categories.py
+++ categories.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
 import db
+import re
 
-print(db.query("insert into CATEGORY (`REGEXP`,`CATEGORY`) values ('.*Visual Studio Code', 'coding')"))
-print(db.query('select * from CATEGORY'))
+categs=db.query('select * from CATEGORY')
+logs=db.query('select * from LOG where CATEGORY is null')
+
+for log in logs:
+    match=False
+    for cat in categs:
+        r='.*%s.*' % cat[1]
+        if re.match(r,log[1]):
+            db.query("update LOG set `CATEGORY`='%s' where `ID`=%d" % (cat[2], log[0]))
+            match=True
+        if not match:
+            print(log)
+
+
