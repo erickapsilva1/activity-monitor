d043fe87af55cd2203b42fe1d68b8a3bace6deb3
Erick Silva <erick.ap.silva@gmail.com>
2021-09-19 09:31:26 -0300

    screeshot feat. developed

--- log.sh
+++ log.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 export DISPLAY=:0.0
+dir=/home/erick/apps/ss/
 
 wname=$(xdotool getactivewindow getwindowname | sed -e "s/'/''/g")
 echo "insert into LOG (\`WINDOW\`,\`USER\`) values ('$wname','$(whoami)');" | mysql --defaults-file=/home/erick/monitor.auth
 
+mkdir -p $dir$(date +%Y-%m-%d)
+import -window root $dir$(date +%Y-%m-%d/)$(date +%Y-%m-%d-%H.%M.%S.jpg)
